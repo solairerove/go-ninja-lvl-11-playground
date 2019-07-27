35386dd41fd3a69a92c32e5ddad2208a5900f428
Mikita Kryvitski <solairerove@gmail.com>
2019-07-27 15:36:31 +0300

    feature: one more custom error example

---
+++ 04-ex/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"log"
+)
+
+type sqrtError struct {
+	lat  string
+	long string
+	err  error
+}
+
+func (se sqrtError) Error() string {
+	return fmt.Sprintf("sqrt error: %v %v %v", se.lat, se.long, se.err)
+}
+
+func main() {
+	_, err := sqrt(-10.23)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func sqrt(f float64) (float64, error) {
+	if f < 0 {
+		e := errors.New("shit happens")
+
+		return 0, sqrtError{
+			lat:  "tyler",
+			long: "durden",
+			err:  e,
+		}
+	}
+
+	return 42, nil
+}
